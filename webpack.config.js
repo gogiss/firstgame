a96bac203689c0fc07bef2a439ab9270351a3eda
Giedrius Tusinskis <giedriustusinskis@gmail.com>
2025-01-12 22:51:25 +0200

    try to change public dir

--- webpack.config.js
+++ webpack.config.js
@@ -24,6 +24,7 @@ module.exports = {
         path: path.resolve(__dirname, "dist"),
         filename: "[name]-[contenthash].bundle.js",
         assetModuleFilename: "asset-packs/[name]-[hash][ext][query]",
+        publicPath: "/firstgame/",
     },
     module: {
         rules: [
