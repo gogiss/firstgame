4ef787c693604f03edb64f85af9839b01a44ce9a
Giedrius Tusinskis <giedriustusinskis@gmail.com>
2025-01-13 23:52:10 +0200

    Revert "separating pure html from phaser games"
    This reverts commit bbed503edd4f23e5c94a5e980fa9156817ea7628.

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
     entry: {
-        // main: "/src/main.js"
+        main: "/src/main.js"
     },
     optimization: {
         splitChunks: {
@@ -29,8 +29,8 @@ module.exports = {
     module: {
         rules: [
             {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader'],
+                test: /\.css$/, // Regex to target CSS files
+                use: ['style-loader', 'css-loader'], // Apply the style-loader and css-loader
                 exclude: /node_modules/,
             },
             {
@@ -69,13 +69,6 @@ module.exports = {
                 {
                     from: path.resolve(__dirname, 'src/assets'),
                     to: 'assets',
-                },
-            ],
-        }),
-        new CopyPlugin({
-            patterns: [
-                {
-                    from: path.resolve(__dirname, 'game.html'),
                 },
             ],
         }),
